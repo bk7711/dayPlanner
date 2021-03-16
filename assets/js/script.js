cfdeb7171a62c0595815119521aa940bed7f102e
C Blackwell <bmathematics@gmail.com>
2021-03-16 09:38:51 -0400

    minor fix

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,14 +13,16 @@ var timerInterval = setInterval(function (){
     
     changeColor();
 
-}, 100);
-
+},5000);
+//save and post user's add to text area when button is clicked
+if(buttonEl.on('click', saveMeeting()));
 
 
 
 
 
 function changeColor() {
+    var i;
     //loop through the array of id's from html
     for (i = 0; i < node.length; i++){
         //set query selector for each id in node array
@@ -30,20 +32,28 @@ function changeColor() {
          //parse the number from the text and save as integer
          integer = parseInt(text,10);
         // compare the integer to the hour of the current time
-             if (now === integer){
-                var change = document.querySelector(node[i]);
-                //if they are equal as current hour change background to red
-                change.addId("present")
-                $( div.node[i] ).siblings().addId("past");
-            }  
+        compare();
 
          }
     };
 
+
+// compare the integer to the hour of the current time
+function compare(){
+    if (now === integer){
+            var change = document.querySelector(node[i]);
+            //if they are equal as current hour change background to red
+            change.setAttribute('class','present');
+            var prevSibling = change.previousElementSibling;
+            prevSibling.setAttribute('class', 'past');
+            var nextSibling = change.nextElementSibling;
+            nextSibling.setAttribute('class', 'future');
+        }  
+};
+
+
 //save and post user's add to text area when button is clicked
 function saveMeeting(){
     var meeting = saveEl.val();
-    buttonEl.on('click', function(){
-        saveEl.apend(meeting);
-    })
-}
+        saveEl.append(meeting);
+    };
